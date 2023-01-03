1b371a81496613edb9366fde39dddf8d93c8a53c
chokj007 <98773970+chokj007@users.noreply.github.com>
2023-01-03 16:23:53 -0500

    Update Index2.js

---
+++ entry-2/Index2.js
@@ -0,0 +1,5 @@
+
+let button = document.querySelector('#activate'); 
+button.onclick = () => { 
+	alert('Disclaimer: Chickens are dumb and scared, they will run...eventually. So is their content! READ FAST '); // Pop an alert!
+};
